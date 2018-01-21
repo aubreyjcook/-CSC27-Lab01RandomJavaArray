063dfc95326420b7913ee071a8d8151c9f113607
aubreyjcook <aubreyjcook@gmail.com>
2018-01-21 12:05:23 -0600

    random array created and initialized with random values

--- src/Lab01RandomJavaArray.java
+++ src/Lab01RandomJavaArray.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
+import java.util.Random;
 
 public class Lab01RandomJavaArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hellow world!");
+		//creates an int array of length 100
+		int[] randomNumberArray = new int[100];
+		
+		//fills the array with random values
+		for (int i = 0; i < 100; i++) {
+			randomNumberArray[i] = (int)(Math.random() * 100 + 1);
+		}
+		
+		//debug note: simple array initialization check
+		/*
+		System.out.println(Arrays.toString(randomNumberArray));
+		*/
+		
 	}
 
 }
