701ee9f9e20aa1175070ef691957708f4420120e
aubreyjcook <aubreyjcook@gmail.com>
2018-01-21 11:39:57 -0600

    public main.java file created

---
+++ src/Lab01RandomJavaArray.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Lab01RandomJavaArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow world!");
+	}
+
+}
