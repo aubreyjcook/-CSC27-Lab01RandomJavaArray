a9e3b0491ee6048e9689f3a6987cc20bc0136145
aubreyjcook <aubreyjcook@gmail.com>
2018-01-25 06:34:39 -0600

    program finalized

--- src/Lab01RandomJavaArray.java
+++ src/Lab01RandomJavaArray.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class Lab01RandomJavaArray {
 
 	public static void main(String[] args) {
 		//creates an int array of length 100
 		int[] randomNumberArray = new int[100];
+		
+		Scanner userInput = new Scanner(System.in);
 		
 		//fills the array with random values
 		for (int i = 0; i < 100; i++) {
@@ -18,6 +18,21 @@ public class Lab01RandomJavaArray {
 		System.out.println(Arrays.toString(randomNumberArray));
 		*/
 		
+		System.out.println("Enter a number between 1 and 100 to check if it exists in the array: ");
+		
+		int value = userInput.nextInt();
+		
+		for (int i = 0; i < 100; i++) {
+			if(randomNumberArray[i] == value) {
+				System.out.println("Your number is matched at index " + (i + 1));
+				break;
+			} else if (i == 99) {
+				System.out.println("No match found.");
+			}
+		}
+		
+		System.out.println("Exiting...");
+		System.exit(0);
 	}
 
 }
